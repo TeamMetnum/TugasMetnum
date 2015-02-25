0b0b38fca794fece71dc70e21c3467fec0ce8002
WIN 8 <WIN 8@192.168.1.101>
2015-02-25 21:37:55 +0700

    perbaikan newton

--- src/METODE/NewtonRhapson.java
+++ src/METODE/NewtonRhapson.java
@@ -1,112 +1,50 @@
 
 package metode;
-import java.util.Scanner;
+import java.util.*;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 public class NewtonRhapson {
-	public static void main (String []args) {
-		Scanner Edhi = new Scanner(System.in);
-		System.out.println ("Pilih jenis fungsi:\na. ax + b\nb. ax^2 + bx + c\nc. ax^3 + bx^2 + cx + d");
-		double[] A = new double[4];
-		double[] dA = new double[3];
-		System.out.print ("Pilihan = ");
-		String pilihan = Edhi.nextLine();
-		int Suku = 0;
-		if (pilihan.equals("a")) Suku = 2;
-		if (pilihan.equals("b")) Suku = 3;
-		if (pilihan.equals("c")) Suku = 4;
-		for (int i = 0; i < Suku; i++) {
-			System.out.print ("koefisien-" + (i + 1) + " = ");
-			A[i] = Edhi.nextDouble();
-		}
-		dA = Turunan(A, Suku);
-		if (Suku == 2) {
-			System.out.println ("f(x) = " + A[0] + "x + " + A[1]);
-			System.out.println ("f'(x) = " + dA[0]);
-		}
-		if (Suku == 3) {
-			System.out.println ("f(x) = " + A[0] + "x^2 + " + A[1] + "x +" + A[2]);
-			System.out.println ("f'(x) = " + dA[0] + "x + " + dA[1]);
-		}
-		if (Suku == 4) {
-			System.out.println ("f(x) = " + A[0] + "x^3 + " + A[1] + "x^2 + " + A[2] + "x + " + A[3]);
-			System.out.println ("f'(x) = " + dA[0] + "x^2 + " + dA[1] + "x + " + A[2]);
-		}
-		System.out.print ("\nIterasi maximum = ");
-		int Iterasi = Edhi.nextInt();
-		System.out.print ("Error = ");
-		double Galat = Edhi.nextDouble();
-		System.out.print ("Nilai X awal = ");
-		double x_0 = Edhi.nextDouble();
-		double Akar = Square(A, dA, Galat, Iterasi, x_0, Suku);
-		System.out.println ("\n\nAkarnya adalah : " + Akar);
+	public static void main (String args []) {
+		Scanner insert = new Scanner (System.in);
+		int iterasi; //menunjukan banyaknya pengulangan yang di inginkan
+		double start, e, Fx, Gx, Hx;
+		double xn = 0;
+		System.out.println ("Masukan fungsi, f(x) = ");
+		String fungsi1 = insert.nextLine();
+		System.out.println ("Masukan turunan dari fungsi f(x), f'(x) = ");
+		String fungsi2 = insert.nextLine();
+		System.out.println ("Masukan banyaknya iterasi yang diinginkan : ");
+		iterasi = insert.nextInt();
+		System.out.println ("Masukan maksimum  toleransi error : ");
+		e = insert.nextDouble();
+		System.out.println ("Masukan nilai awal pendekatan x0 : ");
+		start = insert.nextDouble();
+		
+		for (int i=0 ; i < iterasi ; i++) {
+			Fx = MathParsing (fungsi1, start);
+			Gx = MathParsing (fungsi2, start);
+			/*if (Fx/Gx==tak hingga) {
+				System.out.println ("turunan fungsi f(x) tidak terdefinisi di x ="+start);
+				break;
+			}*/
+			xn = ((start*Gx)-Fx)/Gx;
+			Hx = MathParsing (fungsi1, xn);
+			if (Math.abs(Hx)<e) {
+				break;
+			} else { 
+				start = xn;
+			}
+			
+		} System.out.println ("Solusi x : "+xn);
 	}
 	
-	public static double[] Turunan(double[] A, int Suku) {
-		double[] dA = new double[3];
-		if (Suku == 2) {
-			dA[0] = A[0];
-		}
-		if (Suku == 3) {
-			dA[0] = 2 * A[0];
-			dA[1] = A[1];
-		}
-		if (Suku == 4) {
-			dA[0] = 3 * A[0];
-			dA[1] = 2 * A[1];
-			dA[2] = A[2];
-		}
-		return dA;
+	public static double MathParsing (String func, double var) {
+		Expression e = new ExpressionBuilder (func)
+		.variables("x","e")
+		.build()
+		.setVariable ("x",var)
+		.setVariable ("e", Math.E);
+		
+		return e.evaluate();
 	}
-	
-	public static double Square(double[] A, double[] dA, double Galat, int Iterasi, double x_0, int Suku) {
-		double x_1 = 0, fx = 0, dfx = 0, W = 0, GI = 0;
-		boolean Ketersediaan = false;
-		System.out.println ("i\tXn\tf(Xn)\tf'(Xn)\tf(x)/f'(x)\tXn+1");
-		if (Suku == 2) {
-			for (int i = 0; i <= Iterasi; i++) {
-				if (i != 0) x_0 = x_1;
-				fx = (A[0] * x_0) + A[1];
-				dfx = dA[0];
-				W = fx / dfx;
-				x_1 = x_0 - W;
-				if (x_1 < 0) x_1 = x_1 * (-1);
-				System.out.println (i + "\t" + x_0 + "\t" + fx + "\t" + dfx + "\t" + W + "\t\t" + x_1);
-				GI = x_1 - x_0;
-				if (GI < 0) GI = GI * (-1);
-				if (GI <= Galat) break;
-				if (i == Iterasi) Ketersediaan = true;
-			}
-		}
-		if (Suku == 3) {
-			for (int i = 0; i <= Iterasi; i++) {
-				if (i != 0) x_0 = x_1;
-				fx = (A[0] * (x_0 * x_0)) + (A[1] * x_0) + A[2];
-				dfx = (dA[0] * x_0) + dA[1];
-				W = fx / dfx;
-				x_1 = x_0 - W;
-				if (x_1 < 0) x_1 = x_1 * (-1);
-				System.out.println (i + "\t" + x_0 + "\t" + fx + "\t" + dfx + "\t" + W + "\t\t" + x_1);
-				GI = x_1 - x_0;
-				if (GI < 0) GI = GI * (-1);
-				if (GI <= Galat) break;
-				if (i == Iterasi) Ketersediaan = true;
-			}
-		}
-		if (Suku == 4) {
-			for (int i = 0; i <= Iterasi; i++) {
-				if (i != 0) x_0 = x_1;
-				fx = (A[0] * (x_0 * x_0 * x_0)) + (A[1] * (x_0 * x_0)) + (A[2] * x_0) + A[3];
-				dfx = (dA[0] * (x_0 * x_0)) + (dA[1] * x_0) + dA[2];
-				W = fx / dfx;
-				x_1 = x_0 - W;
-				if (x_1 < 0) x_1 = x_1 * (-1);
-				System.out.println (i + "\t" + x_0 + "\t" + fx + "\t" + dfx + "\t" + W + "\t\t" + x_1);
-				GI = x_1 - x_0;
-				if (GI < 0) GI = GI * (-1);
-				if (GI <= Galat) break;
-				if (i == Iterasi) Ketersediaan = true;
-			}
-		}
-		if (Ketersediaan) System.out.println ("\n\n\nTidak ada/belum ditemukan akar. Akar dibawah ini tidaklah pasti");
-		return x_1;
-	}
-}
