9c6b9564e38c546463dfacee7d0f607bc38ca3ec
WIN 8 <WIN 8@192.168.1.102>
2015-02-27 00:02:41 +0700

    rev secant

--- src/METODE/Secant.java
+++ src/METODE/Secant.java
@@ -1,44 +1,71 @@
 package metode;
+import java.util.*;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
-import java.util.*;
 public class Secant {
-    public static void main(String[] args) {
-        double Fa,Fb,Fx,a,b,x = 0;
-        Scanner input=new Scanner(System.in);
-        System.out.print("Masukkan f(x): "); //1. User menginput f(x)
-        String fungsi=input.nextLine();
-        System.out.print("Masukkan maksimum toleransi error : "); //2. User menginput e
-        double e=input.nextDouble();
-        System.out.print("Masukkan maksimum iterasi (n): "); //3. User menginput n
-        int n=input.nextInt();
-        System.out.print("Masukkan batas bawah (a) : "); //4. User menginput a
-        a=input.nextDouble();
-        System.out.print("Masukkan batas atas (b): "); //5. User menginput b
-        b=input.nextDouble();
-                
-        for(int i=0;i<n;i++){
-            Fa=MathParsing(fungsi,a);
-            Fb=MathParsing(fungsi,b);
-            x=(a*Fb-b*Fa)/(Fb-Fa);
-            Fx=MathParsing(fungsi,x);
-            if(Math.abs(Fx)<e){
+    public static void main(String[] args){
+        Scanner data = new Scanner(System.in);
+        System.out.print("Masukkan fungsi: ");
+        String fungsi = data.nextLine();
+        System.out.print("Toleransi error: ");
+        double error = data.nextDouble();
+        System.out.print("Maksimum iterasi: ");
+        int iterasi = data.nextInt();
+        System.out.print("Batas bawah: ");
+        double batasbawah = data.nextDouble();
+        System.out.print("Batas atas: ");
+        double batasatas = data.nextDouble();
+        SecantM hasilsecant = new SecantM(fungsi, error, iterasi,batasatas, batasbawah);
+        System.out.println(hasilsecant.OutputProcess());
+        System.out.println(hasilsecant.Output());
+    }
+}
+
+class SecantM{
+        String secant_ops;
+        double akar;
+        
+    SecantM(String fx, double er, int n, double a, double b){
+        StringBuilder secant_op = new StringBuilder();
+        akar =0;
+        double fa, fb, fc, c=0;
+        System.out.println();
+        secant_op.append("Proses pencarian akar:\n");
+        secant_op.append("a\t\t\tb\t\t\tfa\t\t\tfb\t\t\tc\t\t\tfc\n");
+        for(int i=0; i<n ; i++){
+            fa = Fy(fx, a);
+            fb = Fy(fx, b);
+            c = (a*fb - b*fa)/(fb-fa);
+            fc = Fy(fx, c);
+            secant_op.append(a+"\t\t\t" +b+"\t\t\t"+fa+"\t\t\t"+fb+"\t\t\t"+c+"\t\t\t"+fc+"\n");
+            if(Math.abs(fc)<er){
                 break;
             }
-            else{
-                a = b; b = x;
+            else {
+                a = b; b = c;
+            } 
             }
-        }
-        System.out.println("Solusi x: "+x);
+        akar = c;
+        secant_ops = secant_op.toString();
     }
-    public static double MathParsing(String func, double var){
-        Expression e = new ExpressionBuilder(func)
-        .variables("x","pi","e")
+
+    
+    String OutputProcess() {
+        return secant_ops;
+    }
+    
+    double Output(){
+        return akar;
+    }
+    
+    public double Fy(String function, double var){
+        Expression e = new ExpressionBuilder(function)
+        .variables("x", "e", "pi")
         .build()
         .setVariable("x", var)
-        .setVariable("pi", Math.PI)
-        .setVariable("e", Math.E);
-
+        .setVariable("e", Math.E)
+        .setVariable("pi", Math.PI);
         return e.evaluate();
     }
 }
+
