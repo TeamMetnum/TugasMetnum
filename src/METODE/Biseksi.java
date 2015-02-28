af85e18d2f4e79a60b080813ef3896e409402ea6
akhlul <>
2015-02-28 18:43:20 +0700

    ubahan yang biseksi

--- src/METODE/Biseksi.java
+++ src/METODE/Biseksi.java
@@ -1,60 +1,134 @@
-package metode ;
-import java.util.Scanner;
+package metode;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+import java.util.*;
+
+/*
+Metode Biseksi :: 60%
+*/   
 
 public class Biseksi {
+    String biseksi_ops; //outputprosess biseksi
+    double Fa, //fx batas atas
+            Fb, //fx batas bawah
+            Fx, //fx saat x
+            a, //x batas bwh
+            b, //x batas ats
+            hx, //hasil akhir biseksi
+            e; //maksimumm toleransi error
+    int n; // maksimum iterasi
+    String fungsi; //fungsi fx
+    boolean atengahb; //apakah a =< solusi =< b
+    
+    public Biseksi(String fungsi_input, double error_input, int maks_input, 
+            double a_input, double b_input ) {
+        StringBuilder biseksi_op = new StringBuilder();
+        
+        double x = 0;
+        String fungsi = fungsi_input;
+        double e = error_input;
+        n = maks_input;
+        
+        a = a_input;
+        b = b_input;
+        
+        Fa = MathParsing(fungsi,a);
+        System.out.println("fa = "+ Fa);
+        Fb = MathParsing(fungsi,b);
+        
+        for (int i=0; i<n; i++) {
+            if(i == 0) {
+                biseksi_op.append("i\t\ta\t\t|b\t\t|x\t\t|Fa\t\t|Fb\t\t|Fx\n");
+                // i    a   b   x   F(a)    F(b)    F(x)
+            }
+            
+            x = (b+a)/2;
+            Fx = MathParsing(fungsi,x);
+            biseksi_op.append(
+                    String.format( i + "\t%.4f\t\t%.4f\t\t%.4f\t\t%.4f\t\t%.4f\t\t%.4f\n", 
+                    a, b, x, Fa, Fb, Fx));
+            
+            if( Math.abs(Fx) < e ) 
+                break;
+            
+            if( Fx * Fa < 0) {
+                b = x;
+                Fb = Fx;
+            } else {
+                a = x;
+                Fa = Fx;
+            }
+        
+        }
+        biseksi_ops = biseksi_op.toString();
+        // solusi nya adalah x yang dihasilkan
+        hx = x;
+    }
+    
+    public String hasil_biseksi() {
+        return String.valueOf(hx);
+    }
+    
+    public String proses_biseksi() {
+        return biseksi_ops;
+    }
+    
+    public void hapusdata() {
+        biseksi_ops = "";
+        Fa = 0;        Fb = 0;        Fx = 0;        n = 0;
+        a = 0;         b = 0;         hx = 0;         e = 0;
+        fungsi = "";
+        atengahb = false;
+    }
+    
+    public boolean antara_ab() {
+        if( MathParsing(fungsi, a) * MathParsing(fungsi,b) <= 0) 
+            return true; 
+        else return false;
+    }
+    public static double MathParsing(String func, double var){
+        Expression a  =  new ExpressionBuilder(func)
+        .variables("x","pi", "e")
+        .build()
+        .setVariable("x", var)
+        .setVariable("pi", Math.PI)
+        .setVariable("e", Math.E);
+        return a.evaluate();
+    }
+}
 
-public static void main(String[] args) {
-Scanner s =new Scanner(System.in);
-Double xr=0.0, Fxr=0.0;
-System.out.printf("Masukkan Batas Bawah : ");
-Double batasbawah = s.nextDouble();
-System.out.printf("Masukan Batas Atas :");
-Double batasatas = s.nextDouble();
-System.out.printf("Maximum Iterasi : ");
-Integer Maximumiterasi = s.nextInt();
-System.out.printf("Galat : ");
-Double galat = s.nextDouble();
-
-Double fa = fungsi(batasbawah);
-Double fb = fungsi(batasatas);
-
-if (fa*fb>0)
-{
-System.out.printf("Tidak ada akar diantara â€œ+batasatas+â€� dan "+batasbawah);
-}
-else
-{
-int kondisi = 1;
-int iterasi = 0;
-while(kondisi == 1)
-{
-iterasi++;
-xr = (batasatas+batasbawah)/2;
-Fxr = fungsi(xr);
-if((Math.abs(Fxr)<galat)|(iterasi>=Maximumiterasi))
-{
-kondisi=0;
+class BiseksiTest {
+    public static void main(String[] args) {
+        double Fa, Fb, Fx, a, b, x = 0;
+        Scanner input = new Scanner(System.in);
+            //1. User menginput f(x)
+        System.out.print("Masukkan f(x): "); 
+        String fungsi = input.nextLine();
+            //2. User menginput e
+        System.out.print("Masukkan maksimum toleransi error : "); 
+        double e = input.nextDouble();
+            //3. User menginput n
+        System.out.print("Masukkan maksimum iterasi (n): "); 
+        int n = input.nextInt();
+            //4. User menginput a
+        System.out.print("Masukkan batas bawah (a) : "); 
+        a = input.nextDouble();
+            //5. User menginput b
+        System.out.print("Masukkan batas atas (b): "); 
+        b = input.nextDouble();
+        
+        Biseksi bi = new Biseksi(fungsi, e, n, a, b);
+        System.out.println("hasil biseksi adalah " + bi.hasil_biseksi());
+        System.out.println(bi.proses_biseksi());
+    }
 }
 
-{
-if(fa*Fxr>0)
-{
-batasbawah = xr;
-fa = Fxr;
-}
-else
-{
-batasatas=xr;
-fb = Fxr;
-}
-}
-}
-System.out.println("Lokasi Akar di "+xr+" dengan "+Fxr);
-}
+/*
+catatan: (karom)
+> kodingnya udh gw beautify dlu, biar caem isinya :D
+> dah ada update terbaru dari si aji, jadi tinggal edit dikit
 
-}
-static double fungsi (double z)
-{
-return Math.exp(-z)-z;
-}
-}
+
+*/
+
