9ecd354f275433ad95fd0084ba951f5a8e7b72af
akhlul <>
2015-02-28 16:18:36 +0700

    baru gw ubah yang iterasu dengan aitken nyya gw ubah namanya aja a :D

--- src/METODE/Iterasi.java
+++ src/METODE/Iterasi.java
@@ -1,17 +1,123 @@
 package metode;
+
 import java.util.*;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
         
 public class Iterasi {
+    StringBuilder iterasi_op = new StringBuilder(""); //output proses sebagai string
+    String fx,
+            finv;
+    double fa,  //fx batas atas
+            fb, //fx batas bawah
+            a,  //x batas atas
+            b,  //x batas bawah
+            EP, //hasil akhir iterasi 
+                //-->> lebih baik gunakan yang string (hasil_iterasi()) 
+                //karena hasilnya kadang tidak bisa didapatkan dalam angka
+            e;  //error
+    int maks; //jumlah iterasi maksimum
     
-    /**
-     * @param args the command line arguments
-     */
+    // dipakai di gui
+    // dalam iterasi, finv maksudnya adalah ketika fx=0 diubah menjadi x=finv(x)
+    public Iterasi(String fx_input, String finv_input, double a_input, double b_input, double error_input, int maks_input) {
+        fx = fx_input;
+        finv = finv_input;
+        a = a_input;
+        b = b_input;
+        e = error_input;
+        maks = maks_input;
+        
+        fa = hasil(fx,a);
+        fb = hasil(fx,b);
+    }
+    
+    // dipakai di gui
+    public String hasil_iterasi(int pilih_input) { 
+        //pilihannya dibuat terserah mau pakai (batas atas == 1) atau (batas bawah = 2)
+        String hasil_sementara = "";
+        if (tes(fa,fb) == true){
+            int pilih = pilih_input;
+            switch (pilih){
+                case 1 : 
+                    iterasi_op.append("no\t\tx\t\tfinv(x)\n");
+                    // no(k)    x   finv(x)
+                    EP = cari_iterasi (finv,a,maks,0,e);
+                    hasil_sementara += "nilai akar x anda adalah dengan batas yang dipakai " + a + " : " + EP;
+                    break;
+                case 2 :
+                    iterasi_op.append("no\t\tx\t\tfinv(x)\n");
+                    // no(k)    x   finv(x)
+                    EP = cari_iterasi (finv,b,maks,0,e);
+                    hasil_sementara += "nilai akar x anda adalah dengan batas yang dipakai " + b + " : " + EP;
+                    break;
+            }
+        } else {
+            hasil_sementara += "maaf batas anda tidak dapat digunakan sebagai dasar iterasi\n";
+            if (tes2(fa,fb) == true)
+                hasil_sementara += " karena salah satu di antara angka anda tersebut adalah akar persamaan";
+        }
+        return hasil_sementara;
+    }
+    
+    // dipakai di gui
+    public String proses_iterasi() {
+        String n = iterasi_op.toString();
+        iterasi_op.setLength(0); // set length of buffer to 0
+        iterasi_op.trimToSize(); // trim the underlying buffer
+        return n;
+        
+    }
+    
+    // dipakai di gui
+    public void hapusdata() {
+        iterasi_op.setLength(0); // set length of buffer to 0
+        iterasi_op.trimToSize(); // trim the underlying buffer
+        fx = "";    finv = ""; 
+        fa = 0;     fb = 0;
+        a = 0;      b = 0;
+        EP = 0;     e = 0;
+        maks = 0;
+    }
+    
+    public double cari_iterasi(String inv, double batas, int ulang, int k, double galat) {
+        k++;
+        double hasilakhir = 0;
+        double nilai = hasil (inv,batas);
+        iterasi_op.append(k + "\t\t" + batas + "\t\t" + nilai + "\n");
+        
+        if ( k == ulang || Math.abs(batas-nilai) <= galat) 
+            hasilakhir = nilai;
+        else {
+             cari_iterasi(inv, nilai, ulang, k, galat);
+        }
+        
+        return hasilakhir;
+    }
+      
+    public double hasil(String func, double var) { // <-- ini adalah MathParsingnya
+        Expression e = new ExpressionBuilder(func)
+        .variables("x")
+        .build()
+        .setVariable("x", var);
+        return e.evaluate();
+    }
+   
+    public boolean tes(double atas, double bawah) {
+        return atas*bawah < 0;
+    }
+    
+    public boolean tes2(double up, double down) {
+        return up*down == 0;
+    }   
+}
+
+class IterasiTest {
     public static void main(String[] args) {
-        Scanner read=new Scanner (System.in);
+        Scanner read = new Scanner(System.in);
         double fa,fb,a,b,EP,e=0;
         int maks;
+        
         System.out.println("===============================");
         System.out.println (" MODULE ITERASI ");
         System.out.println("===============================");
@@ -27,73 +133,14 @@ public class Iterasi {
         e = read.nextDouble();
         System.out.println ("masukan jumlah maksimal iterasi yang anda mau:");
         maks = read.nextInt ();
-        fa= hasil (fx,a);
-        fb= hasil (fx,b);
-        System.out.println("hasil fungsi dari batas bawah anda(f("+a+"))  :"+fa);    
-        System.out.println("hasil fungsi dari batas bawah anda(f("+b+"))  :"+fb);
+                
+        Iterasi iter = new Iterasi(fx, finv, a, b, e, maks);
+        System.out.println(iter.hasil_iterasi(1));
+        System.out.println(iter.proses_iterasi());
         
-        if (tes(fa,fb)== true){
-            System.out.println ("batas anda dapat di gunakan sebagai dasar iterasi");
-            System.out.println ("lanjutkan? 1.yes 2.NO");
-                    int para = read.nextInt();
-                    if (para == 1){
-                        //finv = inverse (fx);
-                        //System.out.println ("fungsi inverse anda:\n" +finv);
-                        //tolong banget benerin fungsi inversenya, kalau ga bisa juga lama lama gw suruh user yang masukin
-                        System.out.println ("batas mana yang mau anda gunakan?\n1.a (batas atas) 2.b (batas bawah)");
-                        int pilih = read.nextInt();
-                        switch (pilih){
-                            case 1 : 
-                                EP =iterasi (finv,a,maks,0,e);
-                                System.out.println("nilai akar x anda adalah :" +EP);
-                                break;
-                            case 2 :
-                                EP =iterasi (finv,b,maks,0,e);
-                                System.out.println("nilai akar x anda adalah :" +EP);
-                                break;
-                                     }
-                    }
-                    else{
-                        System.out.println ("terimakasih");
-                    }
-        }
-        else {
-        System.out.print("maaf batas anda tidak dapat digunakan sebagai dasar iterasi");
-        if (tes2(fa,fb)==true)
-                System.out.println(" karena salah satu di antara angka anda tersebut adalah akar persamaan");
-            }
-    }
-    
-    public static double hasil(String func, double var){
-        Expression e = new ExpressionBuilder(func)
-        .variables("x")
-        .build()
-        .setVariable("x", var);
-        return e.evaluate();
-    }
-   
-    public static boolean tes (double atas, double bawah){
-        return atas*bawah<0;
-        }
-    
-    public static boolean tes2 (double up, double down){
-        return up*down==0;
-        }
-    
-    public static double iterasi (String inv,double batas,int ulang,int k,double galat){
-        k++;
-        double HasilAkir = 0;
-        double nilai = hasil (inv,batas);
-        if (k==ulang|| Math.abs (batas-nilai)>galat) 
-            HasilAkir=nilai;
-        else{
-             iterasi (inv,nilai,ulang,k,galat);}
-        return HasilAkir;
+        System.out.println(iter.hasil_iterasi(2));
+        System.out.println(iter.proses_iterasi());
         
-    }
-    
-    public static String inverse (String a){
-     String c="fuck you";
-     return c;
+        iter.hapusdata();
     }
 }
